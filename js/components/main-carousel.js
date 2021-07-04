d7726591cd21563cb3c3666bb35c1df6cc10e440
lucastdcs <lucas.txsan@gmail.com>
2021-07-04 18:17:16 -0300

    Finalized

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,20 +1,20 @@
 //alert("VAMOS")
 
 
- const btnRight = document.querySelector('.button-arrow.-right')
- const btnLeft = document.querySelector('.button-arrow.-left')
- const elements = document.querySelector('.elements')
- var pixels = 50
+const btnRight = document.querySelector('.button-arrow.-right')
+const btnLeft = document.querySelector('.button-arrow.-left')
+const elements = document.querySelector('.elements')
+var pixels = 50
 
-        btnRight.addEventListener('click', function() {
-            pixels = pixels + 50
-            elements.style = `transform: translateX(${pixels}px) `
-        });
+btnRight.addEventListener('click', function () {
+    pixels = pixels + 50
+    elements.style = `transform: translateX(${pixels}px) `
+});
 
-        
-        btnLeft.addEventListener('click', function() {
-            pixels = pixels - 50
-            elements.style = `transform: translateX(${pixels}px) `
-        });
+
+btnLeft.addEventListener('click', function () {
+    pixels = pixels - 50
+    elements.style = `transform: translateX(${pixels}px) `
+});
 
 
---
+++ js/components/main-form.js
@@ -0,0 +1,9 @@
+//selecionar input button
+//fazer onClick e testar
+
+const text = document.querySelector('main-button.-final')
+
+text.addEventListener('click', function () {
+
+    alert('será?')
+});

87d230f9d0653b94400a8f684fe52e446729ac0a
lucastdcs <lucas.txsan@gmail.com>
2021-07-04 13:57:34 -0300

    Finished carousel

---
+++ js/components/main-carousel.js
@@ -0,0 +1,20 @@
+//alert("VAMOS")
+
+
+ const btnRight = document.querySelector('.button-arrow.-right')
+ const btnLeft = document.querySelector('.button-arrow.-left')
+ const elements = document.querySelector('.elements')
+ var pixels = 50
+
+        btnRight.addEventListener('click', function() {
+            pixels = pixels + 50
+            elements.style = `transform: translateX(${pixels}px) `
+        });
+
+        
+        btnLeft.addEventListener('click', function() {
+            pixels = pixels - 50
+            elements.style = `transform: translateX(${pixels}px) `
+        });
+
+
